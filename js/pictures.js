922e1bbf6994f0fdeec50775c8ad7f72fd826276
MixX0 <31200944+MixX0@users.noreply.github.com>
2018-01-31 21:29:07 +0300

    Добавлена константа

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var QUANTITY_PHOTOS = 25;
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var photoTemplate = document.querySelector('#picture-template').content;
@@ -32,7 +33,7 @@ var getRenderPhoto = function (photo) {
 };
 
 var addPhoto = function (field) {
-  for (var i = 0; i <= 25; i++) {
+  for (var i = 0; i <= QUANTITY_PHOTOS; i++) {
     photoFragment.appendChild(getRenderPhoto(getPhoto(i)));
   }
   field.appendChild(photoFragment);
