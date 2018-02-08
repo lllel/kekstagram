c51be6342141642c60044afc5d3ff23454710d76
MixX0 <31200944+MixX0@users.noreply.github.com>
2018-02-08 16:05:08 +0300

    В процессе

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var QUANTITY_PHOTOS = 25;
+var PHOTOS = [];
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var photoTemplate = document.querySelector('#picture-template').content;
 var photoContainer = document.querySelector('.pictures');
 var indexPhoto = document.querySelector('.gallery-overlay-image');
+var indexPhotoContainer = document.querySelector('.gallery-overlay');
 var indexLikePhoto = document.querySelector('.likes-count');
 var indexPhotoComment = document.querySelector('.comments-count');
 var photoFragment = document.createDocumentFragment();
@@ -23,24 +25,37 @@ var getPhoto = function (i) {
 };
 
 var getRenderPhoto = function (photo) {
-  var templateElement = photoTemplate.cloneNode(true).querySelector('.picture');
+  var templateElement = photoTemplate.querySelector('.picture').cloneNode(true);
 
   templateElement.querySelector('img').src = photo.url;
   templateElement.querySelector('.picture-comments').textContent = photo.likes;
   templateElement.querySelector('.picture-likes').textContent = photo.comments;
 
+  templateElement.addEventListener('click', function () {
+    openedFullSizePhoto(photo);
+  });
+
   return templateElement;
 };
 
-var addPhoto = function (field) {
-  for (var i = 0; i <= QUANTITY_PHOTOS; i++) {
-    photoFragment.appendChild(getRenderPhoto(getPhoto(i)));
+var addPhotoInArray = function () {
+  for (var i = 0; i < QUANTITY_PHOTOS; i++) {
+    PHOTOS.push(getPhoto(i));
   }
+};
+addPhotoInArray();
+
+var addPhoto = function (field) {
+  PHOTOS.forEach(function (el) {
+    photoFragment.appendChild(getRenderPhoto(el));
+  });
   field.appendChild(photoFragment);
 };
 addPhoto(photoContainer);
 
-indexPhoto.src = 'photos/1.jpg';
-indexLikePhoto.textContent = '15';
-indexPhotoComment.textContent = COMMENTS.length;
-document.querySelector('.gallery-overlay').classList.remove('hidden');
+var openedFullSizePhoto = function (photo) {
+  indexPhoto.src = photo.url;
+  indexLikePhoto.textContent = photo.comments;
+  indexPhotoComment.textContent = photo.likes;
+  indexPhotoContainer.classList.remove('hidden');
+};
