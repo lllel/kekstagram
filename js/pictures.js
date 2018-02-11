d0926dd7887f144e24f19e78d7ab7af1cfd1c968
MixX0 <31200944+MixX0@users.noreply.github.com>
2018-02-11 19:08:43 +0300

    Некоторые доработки фильтров

--- js/pictures.js
+++ js/pictures.js
@@ -4,7 +4,6 @@ var QUANTITY_PHOTOS = 25;
 var PHOTOS = [];
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-var photoFragment = document.createDocumentFragment();
 var photoTemplate = document.querySelector('#picture-template').content;
 var photoContainer = document.querySelector('.pictures');
 var formUpload = document.querySelector('.upload-form');
@@ -64,13 +63,17 @@ var addPhotoInArray = function () {
 };
 addPhotoInArray();
 
-var addPhoto = function (field) {
-  PHOTOS.forEach(function (el) {
-    photoFragment.appendChild(getRenderPhoto(el));
+var createElemsFragment = function (arr, cb) {
+  var elemsFragment = document.createDocumentFragment();
+
+  [].forEach.call(arr, function (el, i) {
+    elemsFragment.appendChild(cb(el, i));
   });
-  field.appendChild(photoFragment);
+
+  return elemsFragment;
 };
-addPhoto(photoContainer);
+
+photoContainer.appendChild(createElemsFragment(PHOTOS, getRenderPhoto));
 
 // ОТКРЫТИЕ ПОЛНОРАЗМЕРНОГО ФОТО
 var indexPhoto = document.querySelector('.gallery-overlay-image');
@@ -160,16 +163,78 @@ document.removeEventListener('keydown', onButtonCloseFormEscPress);
 // ИЗМЕНЕНИЕ ЭФФЕКТА ПРЕВЬЮ-ФОТО
 var photoEffectUpload = formUpload.querySelector('.upload-effect-controls');
 var photoEffectPreviewUpload = formUpload.querySelector('.effect-image-preview');
+var valueUpload = formUpload.querySelector('.upload-effect-level-val');
+var pinUpload = formUpload.querySelector('.upload-effect-level-pin');
+var lineUpload = formUpload.querySelector('.upload-effect-level-line');
+var sliderUpload = formUpload.querySelector('.upload-effect-level');
+
+var FilteredStyle = {
+  'effect-none': function () {
+    onHiddenSliderClick();
+
+    photoEffectPreviewUpload.style.filter = '';
+  },
+
+  'effect-chrome': function () {
+    onShowSliderClick();
+
+    photoEffectPreviewUpload.style.filter = 'grayscale(0..1)';
+  },
+
+  'effect-sepia': function () {
+    onShowSliderClick();
+
+    photoEffectPreviewUpload.style.filter = 'sepia(0..1)';
+  },
+
+  'effect-marvin': function () {
+    onShowSliderClick();
+
+    photoEffectPreviewUpload.style.filter = 'invert(0..100%)';
+  },
+
+  'effect-phobos': function () {
+    onShowSliderClick();
+
+    photoEffectPreviewUpload.style.filter = 'blur(0..3px)';
+  },
+
+  'effect-heat': function () {
+    onShowSliderClick();
+
+    photoEffectPreviewUpload.style.filter = 'brightness(0..3)';
+  }
+};
+
+var onShowSliderClick = function () {
+  sliderUpload.style.display = 'block';
+};
+
+var onHiddenSliderClick = function () {
+  sliderUpload.style.display = 'none';
+};
+onHiddenSliderClick();
 
 var onCheckboxEffectChange = function (evt) {
   if (photoEffectPreviewUpload.classList[1]) {
     photoEffectPreviewUpload.classList.remove(photoEffectPreviewUpload.classList[1]);
   }
   photoEffectPreviewUpload.classList.add((evt.target.id).slice(7));
+  FilteredStyle[(evt.target.id).slice(7)]();
+
+  pinUpload.style.left = '100%';
+  valueUpload.style.width = '100%';
 };
+
+pinUpload.style.left = '100%';
+valueUpload.style.width = '100%';
 
 photoEffectUpload.addEventListener('change', function (evt) {
   onCheckboxEffectChange(evt);
+});
+
+pinUpload.addEventListener('mouseup', function () {
+  valueUpload.style.width = (parseInt(getComputedStyle(pinUpload).left, 10) * 100 / parseInt(getComputedStyle(lineUpload).width, 10)).toFixed(0) + '%';
 });
 
 // ИЗМЕНЕНИЕ РЕСАЙЗА ПРЕВЬЮ-ФОТО
@@ -209,14 +274,4 @@ var onButtonResizeValueClick = function (evt) {
 
 uploadResizeContainer.addEventListener('click', function (evt) {
   onButtonResizeValueClick(evt);
-});
-
-// СЛАЙДЕР
-var valueUpload = formUpload.querySelector('.upload-effect-level-value');
-var pinUpload = formUpload.querySelector('.upload-effect-level-pin');
-var lineUpload = formUpload.querySelector('.upload-effect-level-line');
-
-
-pinUpload.addEventListener('mouseup', function () {
-  valueUpload.value = (parseInt(getComputedStyle(pinUpload).left, 10) * 100 / parseInt(getComputedStyle(lineUpload).width, 10)).toFixed(0);
 });
